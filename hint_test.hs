4f5b39d16e2487b0566bf00533226df72cd7ad59
avonb <a.von-berg@uni-muenster.de>
2016-06-29 14:52:50 +0200

    cleaned
    Signed-off-by: avonb <a.von-berg@uni-muenster.de>

--- hint_test.hs
+++ hint_test.hs
@@ -2,53 +2,36 @@ import Control.Monad
 import Language.Haskell.Interpreter
 
 main :: IO ()
-main = do r <- runInterpreter $ compareFunctions "ExampleTemplate" "ExampleCode"
-          case r of
-            Left err -> printInterpreterError err
-            Right () -> putStrLn "Done"
-          c <- runInterpreter $ compareFunctions "ExampleCode" "ExampleTemplate"
+main = do c <- runInterpreter $ compareFunctions "ExampleCode" "ExampleTemplate"
           case c of
-            Left err -> printInterpreterError err
-            Right () -> putStrLn "Done"
+            Left err -> putStrLn $ "Error: " ++ err
+            Right True -> putStrLn "Done Right"
+            Right False -> putStrLn "Done Wrong"
 
-asf asdf 
 subset:: Eq a => [a] -> [a] -> Bool
 subset [] y = True
 subset (x:xs) y = (subset xs y) && (foldr (||) False $ map (\v -> v == x) y )
 
 
-compareFunctions:: String -> String -> Interpreter ()
+
+
+compareFunctions:: String -> String -> Interpreter Bool
 compareFunctions template code = do
     loadModules[template]
     expected <- getModuleExports template
     loadModules[code]
     found <- getModuleExports code
-    say $ show $ subset expected found
+    return $ subset expected found
     
-
-runTestHint :: String -> String -> Interpreter ()
-runTestHint a b =
-    do
-      loadModules [a]
-      loadModules [b]
-      say "Done!"
 
 say :: String -> Interpreter ()
 say = liftIO . putStrLn
 
-printInterpreterError :: InterpreterError -> IO ()
-printInterpreterError e = putStrLn $ "Ups... " ++ (show e)
-
-
-checkModule :: String -> IO ()
+checkModule :: String -> IO Bool
 checkModule name = do 
     r <- runInterpreter $ loadModules [name]
     case r of 
-        Left err -> putStrLn "Error"
-        Right () -> putStrLn "No Error"
+        Left err -> return False
+        Right () -> return True
 
-testHint :: Interpreter ()
-testHint =
-    do
-        loadModules ["SomeModule.hs"]
-        say "Type of Expression:"
+
